e384d0dbf27c5456bdff8dfb1652002198c1a53a
Claudia <cb@uhura.de>
2017-11-08 17:13:28 +0100

    changing text, fixing style

--- js/layout_story_tidy.js
+++ js/layout_story_tidy.js
@@ -624,32 +624,43 @@ squiffy.story.sections = {
 	},
 	'start': {
 		'text': "" +
-        "<article id=\"cb01\" class=\"trs__content-block is-active\">" +
-            "<div class=\"trs__wrapper\">" +
+        "<article id=\"cb01\" class=\"content-block is-active\">" +
+            "<div class=\"wrapper textwrap\">" +
                 "<header class=\"slideInUp\"><h1>Kapitel 1: Ankunft</h1></header>" +
                 "<section>" +
                     "<p class=\"slideInUp slideInUp2\">" +
-                    "Brownie carrot cake chocolate bar chocolate cake I love sweet soufflé cheesecake wafer. Sweet I love icing bonbon" +
-                    " jelly-o pudding pudding marshmallow gummies. Chupa chups chocolate bar sweet roll jelly croissant candy canes" +
-                    " chocolate cake I love. Gummi bears pastry I love marzipan. Dragée tootsie roll carrot cake liquorice brownie" +
-                    " jelly-o gummi bears bonbon. Marshmallow cotton candy muffin chocolate cake macaroon cupcake bear claw. Cotton" +
-                    " candy sesame snaps tart fruitcake gummies. Chupa chups I love I love powder topping pudding. Cupcake" +
-                    " marshmallow brownie oat cake cheesecake. Dessert dragée tart." +
+                        "Alice was beginning to get very tired of sitting by her sister on the bank, and of" +
+                        "having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no" +
+                        "pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?'"+
                     "</p>" +
-                    "<p class=\"slideInUp slideInUp2\">" +
-                    "Soufflé sugar plum pie cookie candy canes jelly. Chocolate oat cake wafer pastry I love toffee. Bear claw " +
-                    "cake pudding powder cake macaroon biscuit cake.</p>" +
-                    "<p class=\"slideInUp slideInUp2\">Liquorice chocolate bar soufflé carrot cake cotton candy marshmallow dragée" +
-                    " oat cake. Ice cream lemon drops macaroon liquorice gingerbread cheesecake sesame snaps jelly caramels. Pie lemon" +
-                    " drops gummies bonbon.</p>" +
-                    "<div class=\"trs__button-wrapper\">" +
+                    "<p class=\"slideInUp slideInUp3\">" +
+                        "So she was considering in her own mind (as well as she could, for the hot day made her feel very " +
+                        "sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and" +
+                        "picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her." +
+                    "</p>" +
+                    "<p class=\"slideInUp slideInUp4\">" +
+                        "There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear " +
+                        "the Rabbit say to itself, 'Oh dear! Oh dear! I shall be late!' (when she thought it over afterwards, it occurred to her " +
+                        "that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took " +
+                        "a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across " +
+                        "her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with " +
+                        "curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge." +
+                    "</p>" +
+                    "<p class=\"slideInUp slideInUp5\">" +
+                        "In another moment down went Alice after it, never once considering how in the world she was to get out again." +
+                    "</p>" +
+                    "<p class=\"slideInUp slideInUp5\">" +
+                        "The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not" +
+                        " a moment to think about stopping herself before she found herself falling down a very deep well." +
+                    "</p>" +
+                    "<div class=\"btnwrap slideInUp slideInUp4\">" +
                         "<a class=\"squiffy-link link-section\" data-section=\"Carkiss yes\" role=\"link\" tabindex=\"0\">Carkiss yes</a>" +
                         "<a class=\"squiffy-link link-section\" data-section=\"Carkiss no\" role=\"link\" tabindex=\"0\">Carkiss no</a>" +
                     "</div>" +
                 "</section>" +
             "</div>" +
-            "<div class=\"trs__image-wrapper\">" +
-                "<img src=\"https://placeimg.com/640/480/any\" class=\"img-responsive\" />" +
+            "<div class=\"imgwrap\">" +
+                "<img src=\"https://placeimg.com/640/480/animals\" class=\"img-responsive\" />" +
             "</div>" +
         "</article>",
 		'passages': {
@@ -657,18 +668,22 @@ squiffy.story.sections = {
 	},
 	'Carkiss yes': {
 		'text': "" +
-        "<article id=\"slide01\" class=\"trs__slide fs\">" +
-            "<div class=\"trs__bcg\"></div>" +
-            "<div class=\"trs__wrapper\">" +
+        "<article id=\"slide01\" class=\"slide fs\">" +
+            "<div class=\"bcg\"></div>" +
+            "<div class=\"wrapper\">" +
                 "<header class=\"slideInUp\"><h1>RENDER #1</h1></header>" +
                 "<section></section>" +
             "</div>" +
         "</article>" +
-        "<article id=\"cb02\" class=\"trs__content-block\">" +
-            "<div class=\"trs__wrapper right\">" +
+        "<article id=\"cb02\" class=\"content-block\">" +
+            "<div class=\"imgwrap\">" +
+                "<img src=\"https://placeimg.com/640/480/animals\" class=\"img-responsive\" />" +
+            "</div>" +
+            "<div class=\"wrapper textwrap right\">" +
                 "<header class=\"slideInUp\"></header>" +
                 "<section>" +
-                    "<p class=\"slideInUp\">Carkisstext YES mit zusätzlichem Bild</p>{Carkiss more}" +
+                    "<p class=\"slideInUp\">Carkisstext YES mit zusätzlichem Bild</p>" +
+                    "{Carkiss more}" +
                 "</section>" +
             "</div>" +
         "</article>",
@@ -678,18 +693,22 @@ squiffy.story.sections = {
 	},
 	'Carkiss no': {
 		'text': "" +
-       "<article id=\"slide01\" class=\"trs__slide fs\">" +
-            "<div class=\"trs__bcg\"></div>" +
-            "<div class=\"trs__wrapper\">" +
+       "<article id=\"slide01\" class=\"slide fs\">" +
+            "<div class=\"bcg\"></div>" +
+            "<div class=\"wrapper\">" +
                 "<header class=\"slideInUp\"><h1>RENDER #1</h1></header>" +
                 "<section></section>" +
             "</div>" +
         "</article>" +
-        "<article id=\"cb02\" class=\"trs__content-block\">" +
-            "<div class=\"trs__wrapper right\">" +
+        "<article id=\"cb02\" class=\"content-block\">" +
+            "<div class=\"imgwrap\">" +
+                "<img src=\"https://placeimg.com/640/480/animals\" class=\"img-responsive\" />" +
+            "</div>" +
+            "<div class=\"wrapper textwrap right\">" +
                 "<header class=\"slideInUp\"></header>" +
                 "<section>" +
-                    "<p class=\"slideInUp\">Carkisstext NO mit zusätzlichem Bild</p>{Carkiss more}" +
+                    "<p class=\"slideInUp\">Carkisstext NO mit zusätzlichem Bild</p>" +
+                    "{Carkiss more}" +
                 "</section>" +
             "</div>" +
         "</article>",
@@ -699,27 +718,135 @@ squiffy.story.sections = {
 	'Carkiss more': {
 		'text': "" +
         "<p class=\"slideInUp slideInUp2\">" +
-        "Brownie carrot cake chocolate bar chocolate cake I love sweet soufflé cheesecake wafer. Sweet I love icing bonbon" +
-        " jelly-o pudding pudding marshmallow gummies. Chupa chups chocolate bar sweet roll jelly croissant candy canes" +
-        " chocolate cake I love. Gummi bears pastry I love marzipan. Dragée tootsie roll carrot cake liquorice brownie" +
-        " jelly-o gummi bears bonbon. Marshmallow cotton candy muffin chocolate cake macaroon cupcake bear claw. Cotton" +
-        " candy sesame snaps tart fruitcake gummies. Chupa chups I love I love powder topping pudding. Cupcake" +
-        " marshmallow brownie oat cake cheesecake. Dessert dragée tart." +
+            "Alice was beginning to get very tired of sitting by her sister on the bank, and of" +
+            "having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no" +
+            "pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?'"+
         "</p>" +
         "<p class=\"slideInUp slideInUp3\">" +
-        "Brownie carrot cake chocolate bar chocolate cake I love sweet soufflé cheesecake wafer. Sweet I love icing bonbon" +
-        " jelly-o pudding pudding marshmallow gummies. Chupa chups chocolate bar sweet roll jelly croissant candy canes" +
-        " chocolate cake I love. Gummi bears pastry I love marzipan. Dragée tootsie roll carrot cake liquorice brownie" +
-        " jelly-o gummi bears bonbon. Marshmallow cotton candy muffin chocolate cake macaroon cupcake bear claw. Cotton" +
-        " candy sesame snaps tart fruitcake gummies. Chupa chups I love I love powder topping pudding. Cupcake" +
-        " marshmallow brownie oat cake cheesecake. Dessert dragée tart." +
+            "So she was considering in her own mind (as well as she could, for the hot day made her feel very " +
+            "sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and" +
+            "picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her." +
         "</p>" +
-        "<div class\"slideInUp slideInUp4\">" +
+        "<p class=\"slideInUp slideInUp4\">" +
+            "There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear " +
+            "the Rabbit say to itself, 'Oh dear! Oh dear! I shall be late!' (when she thought it over afterwards, it occurred to her " +
+            "that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took " +
+            "a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across " +
+            "her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with " +
+            "curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge." +
+        "</p>" +
+        "<p class=\"slideInUp slideInUp5\">" +
+            "In another moment down went Alice after it, never once considering how in the world she was to get out again." +
+        "</p>" +
+        "<p class=\"slideInUp slideInUp5\">" +
+            "The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not" +
+            " a moment to think about stopping herself before she found herself falling down a very deep well." +
+        "</p>" +
+        "<div class\"btnwrap slideInUp slideInUp4\">" +
             "<a class=\"squiffy-link link-section\" data-section=\"Greetingfail yes\" role=\"link\" tabindex=\"0\">Greetingfail yes</a>" +
             "<a class=\"squiffy-link link-section\" data-section=\"Greetingfail no\" role=\"link\" tabindex=\"0\">Greetingfail no</a>" +
         "</div>",
 		'passages': {
 		},
-	},
+    },
+    'Greetingfail yes': {
+        'text': "" +
+        "<article id=\"slide02\" class=\"slide fs\">" +
+            "<div class=\"bcg\"></div>" +
+            "<div class=\"wrapper\">" +
+                "<header class=\"slideInUp\"><h1>RENDER #2</h1></header>" +
+                "<section></section>" +
+            "</div>" +
+        " </article>" +
+        "<article id=\"cb03\" class=\"content-block\">" +
+            "<div class=\"wrapper textwrap\">" +
+                "<header class=\"slideInUp\"></header>" +
+                "<section>" +
+                "<p class=\"slideInUp\">Greetingfail YES mit zusätzlichem Bild</p>" +
+                "{Greetingfail more}" +
+                "</section>" +
+            "</div>" +
+        "<div class=\"imgwrap\">" +
+                "<img src=\"https://placeimg.com/640/480/animals\" class=\"img-responsive\" />" +
+            "</div>" +
+        "</article>",
+        'attributes': ["greetingfail"],
+        'passages': {
+        },
+    },
+    'Greetingfail no': {
+        'text': "" +
+        "<article id=\"slide02\" class=\"slide fs\">" +
+            "<div class=\"bcg\"></div>" +
+            "<div class=\"wrapper\">" +
+                "<header class=\"slideInUp\"><h1>RENDER #2</h1></header>" +
+                "<section></section>" +
+            "</div>" +
+        " </article>" +
+        "<article id=\"cb03\" class=\"content-block\">" +
+            "<div class=\"wrapper textwrap\">" +
+                "<header class=\"slideInUp\"></header>" +
+                "<section>" +
+                    "<p class=\"slideInUp\">Greetingfail NO mit zusätzlichem Bild</p>" +
+                    "{Greetingfail more}" +
+                "</section>" +
+            "</div>" +
+            "<div class=\"imgwrap\">" +
+                "<img src=\"https://placeimg.com/640/480/animals\" class=\"img-responsive\" />" +
+            "</div>" +
+        "</article>",
+        'passages': {
+        },
+    },
+    'Greetingfail more': {
+        'text': "" +
+        "<p class=\"slideInUp slideInUp2\">" +
+            "Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look " +
+            "about her and to wonder what was going to happen next. First, she tried to look down and make out what she was " +
+            "coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they " +
+            "were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs. She took " +
+            "down a jar from one of the shelves as she passed; it was labelled 'ORANGE MARMALADE', but to her great " +
+            "disappointment it was empty: she did not like to drop the jar for fear of killing somebody, so managed to " +
+            "put it into one of the cupboards as she fell past it."+
+        "</p>" +
+        "<p class=\"slideInUp slideInUp3\">" +
+            "'Well!' thought Alice to herself, 'after such a fall as this, I shall think nothing of tumbling down stairs! " +
+            "How brave they'll all think me at home! Why, I wouldn't say anything about it, even if I fell off the top of the " +
+            "house!' (Which was very likely true.)" +
+        "</p>" +
+        "<p class=\"slideInUp slideInUp4\">" +
+            "Down, down, down. Would the fall never come to an end! 'I wonder how many miles I've fallen by this time?' " +
+            "she said aloud. 'I must be getting somewhere near the centre of the earth. Let me see: that would be four thousand " +
+            "miles down, I think—' (for, you see, Alice had learnt several things of this sort in her lessons in the schoolroom, " +
+            "and though this was not a very good opportunity for showing off her knowledge, as there was no one to listen to her, " +
+            "still it was good practice to say it over) '—yes, that's about the right distance—but then I wonder what Latitude " +
+            "or Longitude I've got to?' (Alice had no idea what Latitude was, or Longitude either, but thought they were nice grand words to say.)" +
+        "</p>" +
+        "<p class=\"slideInUp slideInUp5\">" +
+            "Presently she began again. 'I wonder if I shall fall right through the earth! How funny it'll seem to come " +
+            "out among the people that walk with their heads downward! The Antipathies, I think—' (she was rather glad there " +
+            "was no one listening, this time, as it didn't sound at all the right word) '—but I shall have to ask them what the " +
+            "name of the country is, you know. Please, Ma'am, is this New Zealand or Australia?' (and she tried to curtsey as " +
+            "she spoke—fancy curtseying as you're falling through the air! Do you think you could manage it?) 'And what an " +
+            "ignorant little girl she'll think me for asking! No, it'll never do to ask: perhaps I shall see it written up somewhere.'" +
+        "</p>" +
+        "<p class=\"slideInUp slideInUp5\">" +
+            "Down, down, down. There was nothing else to do, so Alice soon began talking again. 'Dinah'll miss me very " +
+            "much to-night, I should think!' (Dinah was the cat.) 'I hope they'll remember her saucer of milk at tea-time. " +
+            "Dinah my dear! I wish you were down here with me! There are no mice in the air, I'm afraid, but you might catch " +
+            "a bat, and that's very like a mouse, you know. But do cats eat bats, I wonder?' And here Alice began to get rather " +
+            "sleepy, and went on saying to herself, in a dreamy sort of way, 'Do cats eat bats? Do cats eat bats?' and sometimes, " +
+            "'Do bats eat cats?' for, you see, as she couldn't answer either question, it didn't much matter which way she put " +
+            "it. She felt that she was dozing off, and had just begun to dream that she was walking hand in hand with Dinah, " +
+            "and saying to her very earnestly, 'Now, Dinah, tell me the truth: did you ever eat a bat?' when suddenly, thump! " +
+            "thump! down she came upon a heap of sticks and dry leaves, and the fall was over." +
+        "</p>" +
+        "<div class\"slideInUp slideInUp4\">" +
+            "<a class=\"squiffy-link link-section\" data-section=\"Antonia_pre\" role=\"link\" tabindex=\"0\">Antonia</a>" +
+            "<a class=\"squiffy-link link-section\" data-section=\"Hisao_pre\" role=\"link\" tabindex=\"0\">Hisao</a>" +
+        "</div>",
+        'passages': {
+        },
+    },
 }
 })();
