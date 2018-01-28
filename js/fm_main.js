39d3c4295637ca13f8c32d4faa6889da42a3c183
Claudia <claudia@email.com>
2018-01-28 17:57:05 +0100

    update

--- js/fm_main.js
+++ js/fm_main.js
@@ -1,23 +1,5 @@
 $(document).ready(function () {
 
-    //// jQuery to collapse the navbar on scroll
-    //function collapseNavbar() {
-    //    if ($(".navbar").offset().top > 50) {
-    //        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    //    } else {
-    //        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    //    }
-    //}
-    //
-    //$(window).scroll(collapseNavbar);
-    //$(document).ready(collapseNavbar);
-    //
-    //// Closes the Responsive Menu on Menu Item Click
-    //$('.navbar-collapse ul li a').click(function() {
-    //    $(this).closest('.collapse').collapse('toggle');
-    //});
-
-
     $("#tr__index__menu__toggle").click(function() {
         $('.tr__index__menu-wrapper').toggleClass("open-menu");
     });
